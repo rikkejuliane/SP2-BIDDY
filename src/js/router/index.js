73c5102f3c69ae6e25505a4f8e0641ee2f54034f
rikkejuliane <rikand01864@stud.noroff.no>
2025-02-03 13:05:18 +0100

    did some changes in the file structure for login and register, i set up tailwind and added some costum properties as colors and fonts

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -9,12 +9,6 @@ export default async function router(pathname = window.location.pathname) {
     case "/auth/":
       await import("./views/auth.js");
       break;
-    case "/auth/login/":
-      await import("./views/login.js");
-      break;
-    case "/auth/register/":
-      await import("./views/register.js");
-      break;
     case "/post/":
       await import("./views/post.js");
       break;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}", "!./node_modules/**/*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Libre Baskerville", "serif"],
+      },
+      colors: {
+        "cloud-grey": "#F6F6F9",
+        "royal-blue": "#0017AD",
+        "lavender-blue": "#9AA2D5",
+        "cardinal-red": "#CA2525",
+        "charcoal-grey": "#4E4E4E",
+      },
+    },
+  },
+  plugins: [],
+};
